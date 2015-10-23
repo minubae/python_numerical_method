1c5ac7abda5b18f6a585b182e6131f16b037dc2e
minwoobae <minubae.nyc@gmail.com>
2015-10-23 16:24:48 -0400

    modified hermite function

--- pynumerical.py
+++ pynumerical.py
@@ -169,7 +169,7 @@ def divided_differences(x, fx):
         return F
 
 ############################ Hermote Interpolation ##############################
-def hermite_interpolate(x, fx, fp):
+def hermite_interpolation(x, fx, fp):
         #x = [1.3, 1.6, 1.9]
         #fx = [0.6200860, 0.4554022, 0.2818186]
         #fp = [-0.5220232, -0.5698959, -0.5811571]
