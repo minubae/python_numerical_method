02765a47d9d823ae53d3907ecde1ef0258b98a27
minwoobae <minubae.nyc@gmail.com>
2015-10-31 13:42:19 -0400

    created euler_method

--- pynumerical.py
+++ pynumerical.py
@@ -194,9 +194,39 @@ def hermite_interpolation(x, fx, fp):
                         
         return Q
 
-############################  Euler's Method ##############################
+
+
+################################  Euler's Method ###################################
+# y(t_i+1) = y(t_i) +h*f(t_i, y(t_i)).
+# w0 = y0; w_i+1 = w_i + h*w(t_i, w_i), for each i = 0,1,...,N-1.
+# To approximate the solution of the initial-value problem, dy/dt = f(t,y), a <= t <= b, y(a)=w0
+# at (N+1) equally spaced numbers in the interval [a,b]:
+# INPUT: Differential equation f(t,y); endpoints a, b; integer N; initial condition y0.
+# OUTPUT: Approximation w to y at the (N+1) values of t.
+def f(t,y):
+    return y/t - (y/t)**2
+
 def euler_method(f, a, b, N, y0):
-    return 
+
+    h = (b-a)/N
+    t = a
+    w = y0
+
+    for i in range(1,N+1):
+        w = w+h*f(t,w)
+        t = a + i*h
+        print('t_'+str(i)+': ', t, 'w_'+str(i)+': ', w)
+        
+    return w
+
+
+
+
+
+
+
+
+
 
 
     
