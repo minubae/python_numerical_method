41a457b87bae2730e47f84c74a2ffa7653030139
minwoobae <minubae.nyc@gmail.com>
2015-11-13 15:24:55 -0500

    modified pynumerical.py

--- pynumerical.py
+++ pynumerical.py
@@ -504,7 +504,7 @@ def runge_kutta(f,a,b,N,y0):
         t = a; w = y0
         print('Initial Value (t0,y0) = ',t,',',w)
 
-        for i in range(1, N):
+        for i in range(1,N+1):
                 K_1 = h*f(t,w)
                 K_2 = h*f(t+h/2, w+K_1/2)
                 K_3 = h*f(t+h/2, w+K_2/2)
@@ -512,7 +512,7 @@ def runge_kutta(f,a,b,N,y0):
 
                 t = a + i*h
                 w = w+(K_1+2*K_2+2*K_3+K_4)/6
-        
+                print('['+str(i)+']','t:',t,'w:',w)
         return t, w
 
 # Runge-Kutta-Fehlberg Method
