91c1d3583884b62d1207b4f50343739a681d8919
minwoobae <minubae.nyc@gmail.com>
2015-11-13 17:23:55 -0500

    modified runge kutta fehlberg

--- pynumerical.py
+++ pynumerical.py
@@ -522,9 +522,8 @@ def runge_kutta(f,a,b,N,y0):
 # minimun step size hmin.
 # OUTPUT: t, w, h where w approximates y(t) and the step size h was used or a message that
 # teh minimum step size was exceeded.
-# Test Command:
-# runge_kutta_fehlberg(f, 0, 2 , 0.5 , 0.00001, 0.25, 0.01)
-def runge_kutta_fehlberg(f, a, b, y0, tol, hmax, hmin ):
+# Test Command: runge_kutta_fehlberg(f, 0, 2 , 0.5 , 0.00001, 0.25, 0.01)
+def runge_kutta_fehlberg(f, a, b, y0, tol, hmax, hmin):
 
         t = a
         w = y0
@@ -537,7 +536,7 @@ def runge_kutta_fehlberg(f, a, b, y0, tol, hmax, hmin ):
 
                 K_1 = h*f(t,w)
                 K_2 = h*f(t+(1/4)*h, w+(1/4)*K_1)
-                K_3 = h*f(t+(3/8)*h, w+(3/32)*K_1+(9/32)*K_2)
+                K_3 = h*f(t+(3/8)*h, w+(3/32)*K_1 + (9/32)*K_2)
                 K_4 = h*f(t+(12/13)*h, w+(1932/2197)*K_1 - (7200/2197)*K_2 + (7296/2197)*K_3)
                 K_5 = h*f(t+h, w+(439/216)*K_1 - 8*K_2 + (3680/513)*K_3 - (845/4104)*K_4)
                 K_6 = h*f(t+(1/2)*h, w - (8/27)*K_1 + 2*K_2 - (3544/2565)*K_3 + (1859/4104)*K_4 - (11/40)*K_5)
@@ -549,7 +548,7 @@ def runge_kutta_fehlberg(f, a, b, y0, tol, hmax, hmin ):
                         w = w + (25/216)*K_1 + (1408/2565)*K_3 + (2197/4104)/K_4 - (1/5)*K_5
                         print('t:', t, 'w:',w, 'h:',h)
 
-                q = 0.84*((tol/R)**(1/4))
+                q = 0.84*(tol/R)**(1/4)
                 print('q:',q)
                 
                 if q <= 0.1:
